cfe23137e742ec7d4b63a011149baca41a7a7636
RaulBirlem <raul.birlem@gmail.com>
2025-02-07 20:59:24 -0300

    Add initial Java program that prints "Olá, Mundo!" and Java version

---
+++ Primeiro/OlaMundo.java
@@ -0,0 +1,9 @@
+package Primeiro;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println("Java version: "+System.getProperty("java.version"));
+    }
+}
+
